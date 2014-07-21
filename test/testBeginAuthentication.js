5f2869b70c0bfab74fd99e522da12c6e9ffb06fb
Joseph Dykstra <josephdykstra@gmail.com>
2014-07-21 13:20:53 -0500

    Update test according to specs

--- test/testBeginAuthentication.js
+++ test/testBeginAuthentication.js
@@ -9,14 +9,36 @@ test('test for beginAuthentication', function(t) {
 	var levelup = Levelup('newThang')
 	var jlc = JustLoginCore(levelup)
 	
-	t.plan(3)
-	jlc.beginAuthentication(fakeId, fakeAddress, function (err) {
-		t.notOk(err, "no error")
+	t.plan(23)
+	jlc.beginAuthentication(fakeId, fakeAddress, function (err, authReqInfo) {
+		t.notOk(err, "O - no error")
+		t.ok(authReqInfo.token, "O - Token exists")
+		t.ok(authReqInfo.contactAddress, "O - Contact Address exists")
+		t.equal(fakeAddress, authReqInfo.contactAddress, "O - Addresses match")
+
+		t.equal(typeof authReqInfo.token, "string", "O - Token is a string")
+		t.equal(typeof authReqInfo.contactAddress, "string", "O - Contact Address is a string")
+
+		jlc.beginAuthentication(fakeId, fakeAddress, function (err, authReqInfo2) {
+			t.notOk(err, "T - no error")
+			t.ok(authReqInfo2.token, "T - Token exists")
+			t.ok(authReqInfo2.contactAddress, "T - Contact Address exists")
+			t.equal(fakeAddress, authReqInfo2.contactAddress, "T - Addresses match")
+
+			t.equal(typeof authReqInfo2.token, "string", "T - Token is a string")
+			t.equal(typeof authReqInfo2.contactAddress, "string", "T - Contact Address is a string")
+
+			t.notEqual(authReqInfo.token, authReqInfo2.token, "T - Default token-gen generates unique strings.")
+		})
 	})
 
 	jlc.on('authentication initiated', function(obj) {
-		t.ok(obj.token, "Token exists")
-		t.equal(obj.contactAddress, fakeAddress, "Adresses match")
+		t.ok(obj.token, "E - Token exists")
+		t.ok(obj.contactAddress, "E - Contact Address exists")
+		t.equal(obj.contactAddress, fakeAddress, "E - Adresses match")
+
+		t.equal(typeof obj.token, "string", "E - Token is a string")
+		t.equal(typeof obj.contactAddress, "string", "E - Contact Address is a string")
 	})
 
 	//-> emits an event with a secret token and the contact address, so somebody can go send a message to that address
