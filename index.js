121bd768790b838e5a133c501d6b781fc451ee79
Joseph Dykstra <josephdykstra@gmail.com>
2015-05-18 19:25:23 -0500

    exports _lock instead of having tokenLock option

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ module.exports = function JustLoginCore(tokenDb, options) {
 	if (!tokenDb) {
 		throw new Error("Just Login Core requires a valid levelup database!")
 	}
-	var map = keyMaster(Mutexify)
+	var lock = keyMaster(Mutexify).get
 	var opts = xtend({
 		tokenGenerator: uuid,
 		tokenTtl: 5 * 60 * 1000, // 5 min
-		tokenTtlCheckIntervalMs: 10 * 1000, // 10 sec
-		tokenLock: map.get
+		tokenTtlCheckIntervalMs: 10 * 1000 // 10 sec
 	}, options)
 
 	ttl(tokenDb, {
@@ -26,8 +25,9 @@ module.exports = function JustLoginCore(tokenDb, options) {
 
 	var emitter = new EventEmitter()
 
-	emitter.authenticate = authenticate(emitter, tokenDb, opts.tokenLock)
-	emitter.beginAuthentication = begin(emitter, tokenDb, opts.tokenLock, opts.tokenGenerator)
+	emitter.authenticate = authenticate(emitter, tokenDb, lock)
+	emitter.beginAuthentication = begin(emitter, tokenDb, lock, opts.tokenGenerator)
+	emitter._lock = lock // tests use this
 
 	return emitter
 }
--- test/testRespectsMutexify.js
+++ test/testRespectsMutexify.js
@@ -1,28 +1,24 @@
 var test = require('tape')
 var JustLoginCore = require('../index.js')
 var Levelup = require('level-mem')
-var keyMaster = require('key-master')
-var Mutexify = require('mutexify')
-
-var map = keyMaster(Mutexify)
 var jlcOpts = {
-	tokenLock: map.get,
 	tokenGenerator: function () {
-		return 'token'
+		return 'token2'
 	}
 }
 
 test('beginAuthentication respects mutexify locks', function(t) {
 	var db = new Levelup()
 	var jlc = JustLoginCore(db, jlcOpts)
+	var lock = jlc._lock
 
-	jlc.beginAuthentication('key-1', 'value', function (err) {
+	jlc.beginAuthentication('token1', 'value', function (err) {
 		t.notOk(err, 'no err ' + (err && err.message))
 		var before = new Date().getTime()
-		map.get('key-2')(function (unlock) {
+		jlc._lock('token2')(function (unlock) {
 			setTimeout(unlock, 200)
 		})
-		jlc.beginAuthentication('key-2', 'value', function (err) {
+		jlc.beginAuthentication('token2', 'value', function (err) {
 			t.notOk(err, 'no error when locked')
 			var elapsed = new Date().getTime() - before
 			t.ok(elapsed >= 195, 'waited long enough: ' + elapsed)
@@ -36,23 +32,25 @@ test('authenticate respects mutexify locks', function(t) {
 	var db = new Levelup()
 	var jlc = JustLoginCore(db, jlcOpts)
 
-	db.put('token1', '{ "contactAddress": "wayToContactPerson", "sessionId": "it doesn\'t care" }')
-	db.put('token2', '{ "contactAddress": "wayToContactPerson", "sessionId": "it doesn\'t care" }')
+	db.put('token1', '{ "contactAddress": "wayToContactPerson", "sessionId": "it doesn\'t care" }', function (err) {
+		t.notOk(err, err && err.message)
+		db.put('token2', '{ "contactAddress": "wayToContactPerson", "sessionId": "it doesn\'t care" }', function (err) {
+			t.notOk(err, err && err.message)
 
-	jlc.authenticate('token1', function (err) {
-		t.notOk(err, 'first auth has no err' + (err && err.message))
-		var before = new Date().getTime()
-		map.get('token2')(function (unlock) {
-			setTimeout(unlock, 200)
-		})
-		setTimeout(function () {
-			jlc.authenticate('token2', function (err) {
-				t.notOk(err, 'no error when locked')
-				var elapsed = new Date().getTime() - before
-				t.ok(elapsed >= 195, 'waited long enough: ' + elapsed)
-				t.ok(elapsed <= 250, 'did not wait too long')
-				t.end()
+			jlc.authenticate('token1', function (err) {
+				t.notOk(err, 'first auth has no err' + (err && err.message))
+				var before = new Date().getTime()
+				jlc._lock('token2')(function (unlock) {
+					setTimeout(unlock, 200)
+				})
+				jlc.authenticate('token2', function (err) {
+					t.notOk(err, 'no error when locked')
+					var elapsed = new Date().getTime() - before
+					t.ok(elapsed >= 195, 'waited long enough: ' + elapsed)
+					t.ok(elapsed <= 250, 'did not wait too long')
+					t.end()
+				})
 			})
-		}, 5)
+		})
 	})
 })
