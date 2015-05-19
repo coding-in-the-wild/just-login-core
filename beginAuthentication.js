568a7ce16390b49e27eec68f0da30f258d24b839
Joseph Dykstra <josephdykstra@gmail.com>
2015-05-18 19:25:11 -0500

    process.nextTick instead of setTimeout

--- beginAuthentication.js
+++ beginAuthentication.js
@@ -3,7 +3,9 @@ module.exports = function b(emitter, tokenDb, tokenLock, tokenGenerator) {
 		if (!cb) cb = function () {}
 
 		if (typeof sessionId !== 'string' || typeof contactAddress !== 'string') {
-			setTimeout(cb, 0, new Error('Session id and/or contact address is not a string.'))
+			process.nextTick(function () {
+				cb(new Error('Session id and/or contact address is not a string.'))
+			})
 		} else {
 			var token = tokenGenerator()
 			var user = JSON.stringify({
